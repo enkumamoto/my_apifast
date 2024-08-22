47618e8d0179db9fa1ceeac594507dfc2ee7ca8c
enkumamoto <eiji.kumamoto@gmail.com>
2024-08-22 07:09:19 -0300

    Upload

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from typing import Union
+import requests
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
@@ -10,9 +21,16 @@ vendas = {
     5: {"item": "garrafa 1L", "preco_unitario": 8, "quantidade": 10},
 }
 
+
 @app.get("/")
 def home():
+    return "Funciona!"
+
+
+@app.get("/vendas")
+def item_vendas():
     return {"Vendas": len(vendas)}
+
 
 @app.get("/vendas/{id_venda}")
 def pegar_venda(id_venda: int):
@@ -20,3 +38,66 @@ def pegar_venda(id_venda: int):
         return vendas[id_venda]
     else:
         return {"error": "Venda não encontrada"}
+
+
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
+
+@app.get("/items0/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/items1/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+
+@app.get("/items2/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item"
+             "that has a long description"}
+        )
+    return item
+
+
+@app.get("/items3/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+
+@app.get("/items4/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
+@app.post("/items5/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item
+
+
+@ app.put("/items6/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
